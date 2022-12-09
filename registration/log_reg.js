68e5fb5559fc9ad9a8a1545880183a35a1dd31e9
George <geo.zarak@gmail.com>
2022-12-10 00:20:54 +0200

    Added password validation

---
+++ registration/log_reg.js
@@ -0,0 +1,38 @@
+var myInput = document.getElementById("Pswr");
+var letter = document.getElementById("characters");
+var capital = document.getElementById("uppercase");
+var symbol  = document.getElementById("symbol");
+var number  = document.getElementById("number");
+
+myInput.onkeyup = function() {
+// Validate lowercase letters
+var lowerCaseLetters = /[a-z]/g;
+if(myInput.value.match(lowerCaseLetters)) {  
+letter.classList.remove("invalid");
+letter.classList.add("valid");
+} else {
+letter.classList.remove("valid");
+letter.classList.add("invalid");
+}
+
+// Validate capital letters
+var upperCaseLetters = /[A-Z]/g;
+if(myInput.value.match(upperCaseLetters)) {  
+capital.classList.remove("invalid");
+capital.classList.add("valid");
+} else {
+capital.classList.remove("valid");
+capital.classList.add("invalid");
+}
+
+// Validate numbers
+var numbers = /[0-9]/g;
+if(myInput.value.match(numbers)) {  
+number.classList.remove("invalid");
+number.classList.add("valid");
+} else {
+number.classList.remove("valid");
+number.classList.add("invalid");
+}
+
+}
