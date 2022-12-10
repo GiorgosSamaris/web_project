b539c73123c99506f12c200e0c5de4b2f443829c
George <geo.zarak@gmail.com>
2022-12-10 17:33:41 +0200

    Completed password validation(sort of)

--- registration/log_reg.js
+++ registration/log_reg.js
@@ -1,38 +1,55 @@
-var myInput = document.getElementById("Pswr");
-var letter = document.getElementById("characters");
-var capital = document.getElementById("uppercase");
+var password = document.getElementById("Pswr");
+var char = document.getElementById("characters");
+var uppercase = document.getElementById("uppercase");
 var symbol  = document.getElementById("symbol");
 var number  = document.getElementById("number");
+var invis = document.getElementById("message_invis");
 
-myInput.onkeyup = function() {
-// Validate lowercase letters
-var lowerCaseLetters = /[a-z]/g;
-if(myInput.value.match(lowerCaseLetters)) {  
-letter.classList.remove("invalid");
-letter.classList.add("valid");
-} else {
-letter.classList.remove("valid");
-letter.classList.add("invalid");
+password.onfocus = function(){
+        document.getElementById("message_invis").id = "message_vis";        //when the user clicks on password field a password validation message is dispalyed
 }
 
-// Validate capital letters
-var upperCaseLetters = /[A-Z]/g;
-if(myInput.value.match(upperCaseLetters)) {  
-capital.classList.remove("invalid");
-capital.classList.add("valid");
-} else {
-capital.classList.remove("valid");
-capital.classList.add("invalid");
+password.onblur = function(){
+        document.getElementById("message_vis").id = "message_invis";        //when user clicks elsewhere the message disappears
 }
 
-// Validate numbers
-var numbers = /[0-9]/g;
-if(myInput.value.match(numbers)) {  
-number.classList.remove("invalid");
-number.classList.add("valid");
-} else {
-number.classList.remove("valid");
-number.classList.add("invalid");
-}
+password.onkeyup = function() {
+    // Validate characters
+    var char_num = /[a-z]/g;
+    if(password.value.match(char_num) && password.value.length >= 8) {  
+        char.classList.remove("invalid");
+        char.classList.add("valid");
+    } else {
+        char.classList.remove("valid");
+        char.classList.add("invalid");
+    }
 
+    // Validate uppercase letter
+    var uprcase = /[A-Z]/g;
+    if(password.value.match(uprcase)) {  
+        uppercase.classList.remove("invalid");
+        uppercase.classList.add("valid");
+    } else {
+        uppercase.classList.remove("valid");
+        uppercase.classList.add("invalid");
+    }
+
+    // Validate numbers
+    var numbers = /[0-9]/g;
+    if(password.value.match(numbers)) {  
+        number.classList.remove("invalid");
+        number.classList.add("valid");
+    } else {
+        number.classList.remove("valid");
+        number.classList.add("invalid");
+    }
+
+    var symbols = /(?=.*[^\w\d\s])/g
+    if(password.value.match(symbols)){
+        symbol.classList.remove("invalid");
+        symbol.classList.add("valid");
+    } else {
+        symbol.classList.remove("valid");
+        symbol.classList.add("invalid");
+    }
 }
