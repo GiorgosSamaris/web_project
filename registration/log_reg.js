d93a5c4a9ba68c3610b5622077da405984aaa648
George <geo.zarak@gmail.com>
2022-12-11 22:14:28 +0200

    Login form turns to registration form

--- registration/log_reg.js
+++ registration/log_reg.js
@@ -4,21 +4,22 @@ var uppercase = document.getElementById("uppercase");
 var symbol  = document.getElementById("symbol");
 var number  = document.getElementById("number");
 var invis = document.getElementById("message_invis");
+var email_vis_btn = document.getElementById("reg");
 
 password.onfocus = function(){
-        document.getElementById("message_invis").id = "message_vis";        //when the user clicks on password field a password validation message is dispalyed
+        document.getElementById("message_inv").id = "message_vis";        //when the user clicks on password field a password validation message is dispalyed
 }
 
 password.onblur = function(){
-        document.getElementById("message_vis").id = "message_invis";        //when user clicks elsewhere the message disappears
+        document.getElementById("message_vis").id = "message_inv";        //when user clicks elsewhere the message disappears
 }
 
 password.onkeyup = function() {
     // Validate characters
     var char_num = /[a-z]/g;
     if(password.value.match(char_num) && password.value.length >= 8) {  
-        char.classList.remove("invalid");
-        char.classList.add("valid");
+        char.classList.remove("invalid");       //if the condition is true then the class of characters is changed to valid(green)
+        char.classList.add("valid");                //same for the below functions but for different elements
     } else {
         char.classList.remove("valid");
         char.classList.add("invalid");
@@ -52,4 +53,9 @@ password.onkeyup = function() {
         symbol.classList.remove("valid");
         symbol.classList.add("invalid");
     }
+}
+
+email_vis_btn.onclick = function(){
+    document.getElementById("email_inv").id = "email_vis";
+    document.getElementById("reg_vis").id = "reg_inv";
 }
