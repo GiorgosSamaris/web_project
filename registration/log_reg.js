1cddd58030dee1d31df21fb3b7643bf31012fa80
George <geo.zarak@gmail.com>
2022-12-12 16:50:40 +0200

    Added colors and moved validation message

--- registration/log_reg.js
+++ registration/log_reg.js
@@ -4,58 +4,64 @@ var uppercase = document.getElementById("uppercase");
 var symbol  = document.getElementById("symbol");
 var number  = document.getElementById("number");
 var invis = document.getElementById("message_invis");
-var email_vis_btn = document.getElementById("reg");
+var reg_btn = document.getElementById("reg");
+var reg_container = document.getElementById("reg_cont");
+var email_btn = document.getElementById("submit");
+var i = 0;
+reg_btn.onclick = function(){
+    document.getElementById("email_cont_inv").id = "email_cont_vis";
+    reg_container.classList.remove("reg_vis");
+    reg_container.classList.add("reg_inv");
+    i = 1;
 
-password.onfocus = function(){
-        document.getElementById("message_inv").id = "message_vis";        //when the user clicks on password field a password validation message is dispalyed
-}
 
-password.onblur = function(){
-        document.getElementById("message_vis").id = "message_inv";        //when user clicks elsewhere the message disappears
-}
-
-password.onkeyup = function() {
-    // Validate characters
-    var char_num = /[a-z]/g;
-    if(password.value.match(char_num) && password.value.length >= 8) {  
-        char.classList.remove("invalid");       //if the condition is true then the class of characters is changed to valid(green)
-        char.classList.add("valid");                //same for the below functions but for different elements
-    } else {
-        char.classList.remove("valid");
-        char.classList.add("invalid");
+    password.onfocus = function(){
+            document.getElementById("message_inv").id = "message_vis";        //when the user clicks on password field a password validation message is dispalyed
     }
-
-    // Validate uppercase letter
-    var uprcase = /[A-Z]/g;
-    if(password.value.match(uprcase)) {  
-        uppercase.classList.remove("invalid");
-        uppercase.classList.add("valid");
-    } else {
-        uppercase.classList.remove("valid");
-        uppercase.classList.add("invalid");
+    
+    password.onblur = function(){
+            document.getElementById("message_vis").id = "message_inv";        //when user clicks elsewhere the message disappears
     }
-
-    // Validate numbers
-    var numbers = /[0-9]/g;
-    if(password.value.match(numbers)) {  
-        number.classList.remove("invalid");
-        number.classList.add("valid");
-    } else {
-        number.classList.remove("valid");
-        number.classList.add("invalid");
+    
+    
+    password.onkeyup = function() {
+        // Validate characters
+        var char_num = /[a-z]/g;
+        if(password.value.match(char_num) && password.value.length >= 8) {  
+            char.classList.remove("invalid");       //if the condition is true then the class of characters is changed to valid(green)
+            char.classList.add("valid");                //same for the below functions but for different elements
+        } else {
+            char.classList.remove("valid");
+            char.classList.add("invalid");
+        }
+    
+        // Validate uppercase letter
+        var uprcase = /[A-Z]/g;
+        if(password.value.match(uprcase)) {  
+            uppercase.classList.remove("invalid");
+            uppercase.classList.add("valid");
+        } else {
+            uppercase.classList.remove("valid");
+            uppercase.classList.add("invalid");
+        }
+    
+        // Validate numbers
+        var numbers = /[0-9]/g;
+        if(password.value.match(numbers)) {  
+            number.classList.remove("invalid");
+            number.classList.add("valid");
+        } else {
+            number.classList.remove("valid");
+            number.classList.add("invalid");
+        }
+    
+        var symbols = /(?=.*[^\w\d\s])/g
+        if(password.value.match(symbols)){
+            symbol.classList.remove("invalid");
+            symbol.classList.add("valid");
+        } else {
+            symbol.classList.remove("valid");
+            symbol.classList.add("invalid");
+        }
     }
-
-    var symbols = /(?=.*[^\w\d\s])/g
-    if(password.value.match(symbols)){
-        symbol.classList.remove("invalid");
-        symbol.classList.add("valid");
-    } else {
-        symbol.classList.remove("valid");
-        symbol.classList.add("invalid");
-    }
-}
-
-email_vis_btn.onclick = function(){
-    document.getElementById("email_inv").id = "email_vis";
-    document.getElementById("reg_vis").id = "reg_inv";
 }
