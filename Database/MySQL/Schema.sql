3112e1488d8fcf230841f7f3eed8b9edd7d0e8e9
Panagiotis Triantafyllopoulos <pantrian04@gmail.com>
2023-07-14 18:17:09 +0300

    Added indexes to tables

--- Database/MySQL/Schema.sql
+++ Database/MySQL/Schema.sql
@@ -14,7 +14,8 @@ CREATE TABLE user (
   isAdmin BOOLEAN DEFAULT FALSE,
   register_date DATETIME DEFAULT now(),
   PRIMARY KEY  (user_id),
-  CONSTRAINT `unq_user_email` UNIQUE(username)
+  CONSTRAINT `unq_user_email` UNIQUE(username),
+  INDEX(username)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
@@ -30,7 +31,8 @@ CREATE TABLE customer (
   overall_score SMALLINT UNSIGNED NOT NULL DEFAULT 0,
   PRIMARY KEY  (customer_id),
   CONSTRAINT `unq_customer_username` UNIQUE(email),
-  CONSTRAINT `fk_customer_client_id` FOREIGN KEY (customer_id) REFERENCES user (user_id) ON DELETE RESTRICT ON UPDATE CASCADE
+  CONSTRAINT `fk_customer_client_id` FOREIGN KEY (customer_id) REFERENCES user (user_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+  INDEX(email)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
@@ -42,7 +44,7 @@ CREATE TABLE category (
   category_id TINYINT UNSIGNED NOT NULL AUTO_INCREMENT,
   name VARCHAR(60) NOT NULL,  
   PRIMARY KEY  (category_id),
-  CONSTRAINT `unq_category_name` UNIQUE(name)
+  CONSTRAINT `unq_category_name` UNIQUE INDEX(name)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
@@ -55,7 +57,8 @@ CREATE TABLE subcategory (
   category_id TINYINT UNSIGNED NOT NULL,
   name VARCHAR(80) NOT NULL,  
   PRIMARY KEY  (subcategory_id),
-  CONSTRAINT `fk_subcategory_category_id)` FOREIGN KEY (category_id) REFERENCES category (category_id) ON DELETE RESTRICT ON UPDATE CASCADE
+  CONSTRAINT `fk_subcategory_category_id)` FOREIGN KEY (category_id) REFERENCES category (category_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+  UNIQUE INDEX(name)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
@@ -97,7 +100,8 @@ CREATE TABLE store (
   longtitude DECIMAL(11,8) NOT NULL,
   latitude DECIMAL(10,8) NOT NULL,
   map_id VARCHAR(16) NOT NULL,
-  PRIMARY KEY  (store_id)
+  PRIMARY KEY  (store_id),
+  INDEX(store_name)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
@@ -112,7 +116,8 @@ CREATE TABLE inventory (
   inventory_price DECIMAL (3,2) NOT NULL DEFAULT 0,
   PRIMARY KEY  (inventory_id),
   CONSTRAINT `fk_inventory_store_id` FOREIGN KEY (store_id) REFERENCES store (store_id) ON DELETE RESTRICT ON UPDATE CASCADE,
-  CONSTRAINT `fk_inventory_product_id` FOREIGN KEY (product_id) REFERENCES product (product_id) ON DELETE RESTRICT ON UPDATE CASCADE
+  CONSTRAINT `fk_inventory_product_id` FOREIGN KEY (product_id) REFERENCES product (product_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+  INDEX (store_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
@@ -135,7 +140,8 @@ CREATE TABLE offer (
   PRIMARY KEY  (offer_id),
   CONSTRAINT `fk_offer_store_id` FOREIGN KEY (store_id) REFERENCES store (store_id) ON DELETE RESTRICT ON UPDATE CASCADE,
   CONSTRAINT `fk_offer_product_id` FOREIGN KEY (product_id) REFERENCES product (product_id) ON DELETE RESTRICT ON UPDATE CASCADE,
-  CONSTRAINT `fk_offer_customer_id` FOREIGN KEY (customer_id) REFERENCES customer (customer_id) ON DELETE RESTRICT ON UPDATE CASCADE
+  CONSTRAINT `fk_offer_customer_id` FOREIGN KEY (customer_id) REFERENCES customer (customer_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+  INDEX(customer_id, store_id, product_id, active)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
