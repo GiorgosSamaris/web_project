614cbb80e786b1e6fc6f8589c8a85d3c163738d6
Panagiotis Triantafyllopoulos <pantrian04@gmail.com>
2023-07-15 18:03:02 +0300

    Updated price datatype

--- Database/MySQL/Schema.sql
+++ Database/MySQL/Schema.sql
@@ -57,7 +57,7 @@ CREATE TABLE subcategory (
   category_id TINYINT UNSIGNED NOT NULL,
   name VARCHAR(80) NOT NULL,  
   PRIMARY KEY  (subcategory_id),
-  CONSTRAINT `fk_subcategory_category_id)` FOREIGN KEY (category_id) REFERENCES category (category_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+  CONSTRAINT `fk_subcategory_category_id` FOREIGN KEY (category_id) REFERENCES category (category_id) ON DELETE RESTRICT ON UPDATE CASCADE,
   UNIQUE INDEX(name)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -113,7 +113,7 @@ CREATE TABLE inventory (
   inventory_id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
   store_id TINYINT UNSIGNED NOT NULL,
   product_id SMALLINT UNSIGNED NOT NULL,
-  inventory_price DECIMAL (3,2) NOT NULL DEFAULT 0,
+  inventory_price DECIMAL (4,2) NOT NULL DEFAULT 0,
   PRIMARY KEY  (inventory_id),
   CONSTRAINT `fk_inventory_store_id` FOREIGN KEY (store_id) REFERENCES store (store_id) ON DELETE RESTRICT ON UPDATE CASCADE,
   CONSTRAINT `fk_inventory_product_id` FOREIGN KEY (product_id) REFERENCES product (product_id) ON DELETE RESTRICT ON UPDATE CASCADE,
@@ -130,7 +130,7 @@ CREATE TABLE offer (
   product_id SMALLINT UNSIGNED NOT NULL,
   store_id TINYINT UNSIGNED NOT NULL,
   customer_id SMALLINT UNSIGNED NOT NULL,
-  offer_price DECIMAL (3,2) NOT NULL DEFAULT 0,
+  offer_price DECIMAL (4,2) NOT NULL DEFAULT 0,
   number_of_likes SMALLINT UNSIGNED NOT NULL DEFAULT 0,
   number_of_dislikes SMALLINT UNSIGNED NOT NULL DEFAULT 0,
   active BOOLEAN NOT NULL DEFAULT TRUE,
@@ -143,6 +143,7 @@ CREATE TABLE offer (
   CONSTRAINT `fk_offer_customer_id` FOREIGN KEY (customer_id) REFERENCES customer (customer_id) ON DELETE RESTRICT ON UPDATE CASCADE,
   INDEX(customer_id, store_id, product_id, active)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 
 
 
--- Database/MySQL/Stored_Procedures/new_offer.sql
+++ Database/MySQL/Stored_Procedures/new_offer.sql
@@ -2,24 +2,23 @@
 
 DROP PROCEDURE IF EXISTS new_offer;
 DELIMITER $
-CREATE PROCEDURE new_offer(IN pro_id SMALLINT UNSIGNED, IN sto_id TINYINT UNSIGNED, IN cus_id SMALLINT UNSIGNED, IN price DECIMAL(3,2))
+CREATE PROCEDURE new_offer(IN pro_id SMALLINT UNSIGNED, IN sto_id TINYINT UNSIGNED, IN cus_id SMALLINT UNSIGNED, IN price DECIMAL(4,2))
 BEGIN
 DECLARE gain INT;
-DECLARE avg_price DECIMAL(3,2);
+DECLARE avg_price DECIMAL(4,2);
 
 INSERT INTO `offer`(`product_id`, `store_id`, `customer_id`, `offer_price`) VALUES(pro_id, sto_id, cus_id, price);
 UPDATE offer SET expiration_date = DATE_ADD(expiration_date, INTERVAL 7 DAY) WHERE offer_id = LAST_INSERT_ID();
 CALL get_yester_price(pro_id, avg_price);
-
+SET gain = 0;
 IF(avg_price*0.8 > price) THEN
     SET gain = 50;
 ELSE 
-    SET gain = 0;
     CALL get_last_week_price(pro_id, avg_price);
     IF (avg_price*0.8 > price) THEN
         SET gain = 20;
     END IF;
 END IF;
-UPDATE customer SET current_score = current_score + gain WHERE customer_id = cus_id;
+UPDATE customer SET current_score = current_score + gain, overall_score = overall_score + gain WHERE customer_id = cus_id;
 END$
 DELIMITER ;
