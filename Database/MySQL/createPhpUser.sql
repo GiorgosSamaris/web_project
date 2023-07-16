9aa7aa6b58e29d5b249ef0f46ce1f8c6d437ff76
Panagiotis Triantafyllopoulos <pantrian04@gmail.com>
2023-07-16 15:57:11 +0300

    Added arguments to connect to azure

--- Database/MySQL/createPhpUser.sql
+++ Database/MySQL/createPhpUser.sql
@@ -5,3 +5,5 @@ GRANT INSERT ON GoCart.category TO 'phpClient'@'localhost';
 GRANT INSERT ON GoCart.subcategory TO 'phpClient'@'localhost';
 GRANT INSERT ON GoCart.price_history TO 'phpClient'@'localhost';
 GRANT INSERT ON GoCart.store TO 'phpClient'@'localhost';
+
+grant execute on GoCart.* to 'phpClient'@'localhost';
--- Database/php/categoryProductInsert.php
+++ Database/php/categoryProductInsert.php
@@ -1,18 +1,22 @@
 <?php
 //connection arguments
-$servername = "localhost";
-$username = "phpClient";
-$password = "$0ftK1ngsPhP";
-$dbname = "GoCart";
+// $servername = "localhost";
+// $username = "phpClient";
+// $password = "$0ftK1ngsPhP";
+// $dbname = "GoCart";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+// // Create connection
+// $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
-if ($conn->connect_error){
-    die("Connection failed: " . $conn->connect_error);
-    print_r("OH NO!");
-}
+// // Check connection
+// if ($conn->connect_error){
+//     die("Connection failed: " . $conn->connect_error);
+//     print_r("OH NO!");
+// }
+
+$conn = mysqli_init();
+mysqli_ssl_set($conn,NULL,NULL, "/home/ptriantafy/Downloads/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+mysqli_real_connect($conn, "gocart.mysql.database.azure.com", "goCartDevTeam", "softk1ng\$d3v", "gocart", 3306, MYSQLI_CLIENT_SSL);
 
 $file = file_get_contents('json/dumpProducts.json');
 $parsed_file = json_decode($file, true);
