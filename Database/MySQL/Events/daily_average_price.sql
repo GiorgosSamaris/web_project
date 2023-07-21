394f221eccde5ffbb0ba76f59ae5732268cf858a
Panagiotis Triantafyllopoulos <pantrian04@gmail.com>
2023-07-21 19:45:43 +0300

    Added event to calculate daily_average

---
+++ Database/MySQL/Events/daily_average_price.sql
@@ -0,0 +1,21 @@
+DROP EVENT IF EXISTS daily_average_price;
+
+-- Calculates daily average price for each product and deletes old entries (>8days)
+
+DELIMITER $
+CREATE EVENT daily_average_price
+ON SCHEDULE EVERY 1 DAY 
+STARTS '2023-07-22 01:00:00'
+COMMENT 'Calculates daily average price for each product and deletes old entries (>8days)'
+DO 
+BEGIN
+INSERT INTO price_history (product_id, price_date, average_price)
+SELECT
+  product_id,
+  CURDATE() AS price_date,
+  AVG(inventory_price) AS average_price
+FROM inventory
+GROUP BY product_id;
+END$
+
+DELIMITER ;
