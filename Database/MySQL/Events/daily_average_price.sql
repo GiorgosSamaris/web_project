da1137e23138bab37d21414243562c4048e67149
Panagiotis Triantafyllopoulos <pantrian04@gmail.com>
2023-07-22 11:58:29 +0300

    Modified event times

--- Database/MySQL/Events/daily_average_price.sql
+++ Database/MySQL/Events/daily_average_price.sql
@@ -5,7 +5,7 @@ DROP EVENT IF EXISTS daily_average_price;
 DELIMITER $
 CREATE EVENT daily_average_price
 ON SCHEDULE EVERY 1 DAY 
-STARTS '2023-07-22 01:00:00'
+STARTS '2023-07-22 03:00:00'
 COMMENT 'Calculates daily average price for each product and deletes old entries (>8days)'
 DO 
 BEGIN
