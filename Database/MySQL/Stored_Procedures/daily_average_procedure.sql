50bc7c48f9e1c568692183eeb89c0658c40d68da
Panagiotis Triantafyllopoulos <pantrian04@gmail.com>
2023-08-01 18:07:18 +0300

    added delete where price = 0, occurs rarely #48

--- Database/MySQL/Stored_Procedures/daily_average_procedure.sql
+++ Database/MySQL/Stored_Procedures/daily_average_procedure.sql
@@ -9,5 +9,7 @@ SELECT
   ROUND(AVG(inventory_price),2) AS average_price
 FROM inventory
 GROUP BY product_id;
+-- hopefully this doesn't cause more problems than it solves
+DELETE FROM price_history WHERE average_price = 0;
 END$
 DELIMITER ;
