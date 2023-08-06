138850d904e464ac6d81292861300a00d2f6443c
Panagiotis Triantafyllopoulos <pantrian04@gmail.com>
2023-08-06 17:42:35 +0300

    added select privileges for more tables

--- Database/MySQL/createPhpUser.sql
+++ Database/MySQL/createPhpUser.sql
@@ -7,6 +7,13 @@ GRANT INSERT ON GoCart.subcategory TO 'phpClient'@'localhost';
 GRANT INSERT ON GoCart.price_history TO 'phpClient'@'localhost';
 GRANT INSERT ON GoCart.store TO 'phpClient'@'localhost';
 GRANT INSERT ON GoCart.temp_table TO 'phpClient'@'localhost';
-GRANT SELECT ON GoCart.user TO 'phpClient'@'localhost';
 GRANT SELECT ON GoCart.customer TO 'phpClient'@'localhost';
+GRANT SELECT ON GoCart.user TO 'phpClient'@'localhost';
+GRANT SELECT ON GoCart.offer TO 'phpClient'@'localhost';
+GRANT SELECT ON GoCart.product TO 'phpClient'@'localhost';
+GRANT SELECT ON GoCart.subcategory TO 'phpClient'@'localhost';
+GRANT SELECT ON GoCart.category TO 'phpClient'@'localhost';
+
+
+GRANT SELECT ON GoCart.store TO 'phpClient'@'localhost';
 grant execute on GoCart.* to 'phpClient'@'localhost';
