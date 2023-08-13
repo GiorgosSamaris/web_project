eb793388b7febbc059c63eb87b8afbeab4fecc4c
Panagiotis Triantafyllopoulos <pantrian04@gmail.com>
2023-08-13 16:09:23 +0300

    added product images and resized them to 65px width

--- Database/images/resize_images.py
+++
@@ -1,7 +0,0 @@
-import os
-rootdir = '/home/ptriantafy/Documents/Project Web Development/Database/images/img'
-
-for subdir, dirs, files in os.walk(rootdir):
-    for file in files:
-        print("Resizing: "+os.path.join(subdir, file)+"...")
-    print("OK!")
