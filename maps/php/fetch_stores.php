44e7aac1dc881373b08ef2d6716f1b6b2c97eedc
Panagiotis Triantafyllopoulos <pantrian04@gmail.com>
2023-08-16 17:27:47 +0300

    added relative path

--- maps/php/fetch_stores.php
+++ maps/php/fetch_stores.php
@@ -1,18 +1,19 @@
 <?php 
 header('Content-Type: application/json; charset=utf-8');
-$servername = "localhost";
-$username = "phpClient";
-$password = "$0ftK1ngsPhP";
-$dbname = "GoCart";
+include(dirname(__DIR__).'/../azureConnection/azureConn.php');
+// $servername = "localhost";
+// $username = "phpClient";
+// $password = "$0ftK1ngsPhP";
+// $dbname = "GoCart";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-$conn->set_charset("utf8");
-// Check connection
-if ($conn->connect_error){
-    die("Connection failed: " . $conn->connect_error);
-    print_r("OH NO!");
-}
+// // Create connection
+// $conn = new mysqli($servername, $username, $password, $dbname);
+// $conn->set_charset("utf8");
+// // Check connection
+// if ($conn->connect_error){
+//     die("Connection failed: " . $conn->connect_error);
+//     print_r("OH NO!");
+// }
 
 $result = $conn->query("SELECT * FROM store;");
 $features = array();
