9649ae6f357f86ce3ef937de6b46828e6d10e60d
GiorgosSamaris <georgelmt10219@gmail.com>
2023-08-19 21:56:03 +0300

    Stored procedures for changing username and password

---
+++ Database/MySQL/Stored_Procedures/change_username.sql
@@ -0,0 +1,14 @@
+DROP PROCEDURE IF EXISTS change_username;
+DELIMITER $
+CREATE PROCEDURE change_username(IN n_username VARCHAR(45), IN id SMALLINT UNSIGNED)
+BEGIN
+    DECLARE username_exists SMALLINT UNSIGNED;
+    SELECT COUNT(*) INTO username_exists FROM user WHERE username = n_username;
+    IF (username_exists = 0) THEN
+        UPDATE user SET username = n_username WHERE user_id = id;
+        SELECT "1";
+    ELSE
+        SELECT "0"; #user already_exitsts
+    END IF;
+END$
+DELIMITER ;
