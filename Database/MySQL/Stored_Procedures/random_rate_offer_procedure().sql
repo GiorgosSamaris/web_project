7827e1dc7b5cf27ce2b9098b4cb9f1ab2afc03b4
Panagiotis Triantafyllopoulos <pantrian04@gmail.com>
2023-08-21 16:07:55 +0300

    added out in_stock in select statement

--- Database/MySQL/Stored_Procedures/random_rate_offer_procedure().sql
+++ Database/MySQL/Stored_Procedures/random_rate_offer_procedure().sql
@@ -6,7 +6,7 @@ BEGIN
     DECLARE off_id MEDIUMINT UNSIGNED;
     DECLARE cust_id SMALLINT UNSIGNED;
     -- fetch required values
-    SELECT offer_id, author_id FROM offer WHERE active = true ORDER BY RAND() LIMIT 1 INTO off_id, cust_id;
+    SELECT offer_id, author_id FROM offer WHERE active = true AND in_stock = true ORDER BY RAND() LIMIT 1 INTO off_id, cust_id;
     SELECT customer_id FROM customer WHERE customer_id != cust_id ORDER BY RAND() LIMIT 1 INTO cust_id;
     -- generate a random number in [1, 10], if it's >= 5 then like the offer, else dislike
     IF 5 >= FLOOR( 1 + (RAND() * 10)) THEN
