43c75d424ed51c47ce13fdc71dfd9cb3a6f06a2e
Panagiotis Triantafyllopoulos <pantrian04@gmail.com>
2023-09-07 04:12:47 +0300

    fetches distinct categories and hasActiveOffers bool

--- maps/php/fetch_stores.php
+++ maps/php/fetch_stores.php
@@ -1,34 +1,41 @@
-<?php 
+<?php
 header('Content-Type: application/json; charset=utf-8');
-include(dirname(__DIR__).'/../azureConnection/azureConn.php');
-// $servername = "localhost";
-// $username = "phpClient";
-// $password = "$0ftK1ngsPhP";
-// $dbname = "GoCart";
-
-// // Create connection
-// $conn = new mysqli($servername, $username, $password, $dbname);
-// $conn->set_charset("utf8");
-// // Check connection
-// if ($conn->connect_error){
-//     die("Connection failed: " . $conn->connect_error);
-//     print_r("OH NO!");
-// }
+include(dirname(__DIR__) . '/../azureConnection/azureConn.php');
 
 $result = $conn->query("SELECT * FROM store;");
 $features = array();
-// foreach row
+
 while ($row = $result->fetch_assoc()) {
+    // Query the distinct categories in the store's inventory
+    $inventoryQuery = "SELECT DISTINCT c.name AS category_name 
+                       FROM inventory i
+                       INNER JOIN product p ON i.product_id = p.product_id
+                       INNER JOIN subcategory s ON p.subcategory_id = s.subcategory_id
+                       INNER JOIN category c ON s.category_id = c.category_id
+                       WHERE i.store_id = {$row['store_id']}";
+    $inventoryResult = $conn->query($inventoryQuery);
+
+    // Create an array to store distinct categories
+    $distinctCategories = array();
+    while ($inventoryRow = $inventoryResult->fetch_assoc()) {
+        $distinctCategories[] = $inventoryRow['category_name'];
+    }
+
     $feature = array(
         'type' => 'Feature',
         'geometry' => array(
             'type' => 'Point',
-            // cast to float
-            'coordinates' => array((float)$row['longitude'], (float)$row['latitude']),
+            'coordinates' => array((float) $row['longitude'], (float) $row['latitude']),
         ),
-        'properties' => $row,
+        'properties' => array(
+            'store_id' => $row['store_id'],
+            'store_name' => $row['store_name'],
+            'has_active_offers' => checkIfStoreHasActiveOffers($conn, $row['store_id']), // Add the boolean property
+            'address' => $row['address'],
+            'distinct_categories' => $distinctCategories, // Add the array of distinct categories
+            // Add other properties here
+        ),
     );
-    // append to array
     $features[] = $feature;
 }
 
@@ -38,6 +45,16 @@ $geojson = array(
 );
 
 $json = json_encode($geojson, JSON_UNESCAPED_UNICODE);
-file_put_contents(__DIR__.'/../json/stores.geojson', $json);
-// echo $json;
+file_put_contents(__DIR__ . '/../json/stores.geojson', $json);
+
+function checkIfStoreHasActiveOffers($conn, $storeID)
+{
+    // Query the 'Offer' table to check if the store has active offers
+    $query = "SELECT COUNT(*) AS active_offer_count FROM offer WHERE store_id = $storeID AND active = TRUE";
+    $result = $conn->query($query);
+    if ($result && $row = $result->fetch_assoc()) {
+        return $row['active_offer_count'] > 0;
+    }
+    return false;
+}
 ?>
