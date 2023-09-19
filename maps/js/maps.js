590d14516ad866eb9422b60bc9423975c0ee41dc
Panagiotis Triantafyllopoulos <pantrian04@gmail.com>
2023-09-19 20:56:32 +0300

    admin bug fix

--- maps/js/maps.js
+++ maps/js/maps.js
@@ -13,10 +13,9 @@ let markersLayer = new L.LayerGroup().addTo(mymap);
 let hiddenLayer = new L.LayerGroup(); 
 let offersList = []; 
 let profileContainer;
-let isAdmin = sessionStorage.getItem("isAdmin");
-let userId = parseInt(sessionStorage.getItem("userId"));
+var isAdmin = (sessionStorage.getItem("isAdmin")=== "true");
+var userId = parseInt(sessionStorage.getItem("userId"));
 // let userId = 100; //comment this out when testing is done, uncomment line 14
-
 function updateBodyHeightWidth(){   //change body width and height to fit the screen
     height = window.innerHeight + "px";
     width = window.innerWidth + "px";
